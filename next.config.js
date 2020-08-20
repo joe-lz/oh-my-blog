9ce8f177c3cf2e1f014999234f3d3350136c4cb6
joe-lz <joe-lz@users.noreply.github.com>
2020-08-20 08:33:00 +0000

    deploy: 5660956bc8d039f3717f2b05efedb3a5d4eecff5

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     
     YOUR_LEANCLOUD_APPID: "xac008ks23rBMea1NYnOqJme-MdYXbMMI", // leancloud appid
     YOUR_LEANCLOUD_APPKEY: "9BYXDRq8MWTCBoiLdiICqbeh", // leancloud appkey
-    YOUR_LEANCLOUD_SERVER_URL: "https://leanblog-api.todokit.xyz", //leancloud serverUrl
+    YOUR_LEANCLOUD_SERVER_URL: "https://omb-api.jser.blog", //leancloud serverUrl
 
     blogname: "动态模块",
   },
