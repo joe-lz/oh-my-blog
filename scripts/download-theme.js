b217315ee6ce13414e14a882f0f02e5ea42c9e3c
joe-lz <joe-lz@users.noreply.github.com>
2020-08-21 05:04:09 +0000

    deploy: dec62ced9abe326c10308f8d19476ea98f25f2cc

--- scripts/download-theme.js
+++ scripts/download-theme.js
@@ -1,27 +1,30 @@
-const download = require('download-git-repo')
-const fs = require('fs')
-const rimraf = require('rimraf')
-const config = require('../omb.config.js')
+const download = require("download-git-repo");
+const fs = require("fs");
+const rimraf = require("rimraf");
+const config = require("../omb.config.js");
 
-download('joe-lz/ombTheme-white-admin', 'temp_theme/admin', async (err) => {
-  await copyFolder({ oldPath: 'temp_theme/admin/pages', newPath: 'src/pages/admin' })
-  await copyFolder({ oldPath: 'temp_theme/admin/components', newPath: 'src/components/admin' })
-
-  download('joe-lz/ombTheme-white-www', 'temp_theme/www', async (err) => {
-    await copyFolder({ oldPath: 'temp_theme/www/pages', newPath: 'src/pages/www' })
-    await copyFolder({ oldPath: 'temp_theme/www/components', newPath: 'src/components/www' })
-  })
-})
+Object.keys(config.theme).map((themeName) => {
+  download(config.theme[themeName], `temp_theme/${themeName}`, async (err) => {
+    await copyFolder({
+      oldPath: `temp_theme/${themeName}/pages`,
+      newPath: `src/pages/${themeName}`,
+    });
+    await copyFolder({
+      oldPath: `temp_theme/${themeName}/components`,
+      newPath: `src/components/${themeName}`,
+    });
+  });
+});
 
 const copyFolder = ({ oldPath, newPath }) => {
   return new Promise((resolve, reject) => {
-    rimraf.sync(newPath)
+    rimraf.sync(newPath);
     fs.rename(oldPath, newPath, (err) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve()
+        resolve();
       }
-    })
-  })
-}
+    });
+  });
+};
