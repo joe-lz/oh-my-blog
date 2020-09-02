5dd1e7948b5b30314f7424c9b92eece60fb2f5fa
bangdong.chen <bangdong.chen@crgrcent.com>
2020-09-02 09:37:11 +0800

    fixed:  add views

--- src/service/profile.js
+++ src/service/profile.js
@@ -1,57 +1,67 @@
-import AV from 'leancloud-storage'
+import AV from "leancloud-storage";
 
 export const createProfile = (params = {}) => {
   return new Promise((resolve, reject) => {
     // 创建默认菜单
-    const profileObject = new AV.Object('CMS_Profile')
-    profileObject.set('logo', 'https://static.ccrgt.com/images/42bc258e-d183-49d5-aea4-6eb5914647e3.png')
-    profileObject.set('title', 'oh my blog')
-    profileObject.set('github', 'https://github.com/joe-lz/oh-my-blog')
-    profileObject.set('copyright', 'Copyright © 1998 - 2020 oh my blog. All Rights Reserved')
-    profileObject.set('allowAComment', true)
-    profileObject.set('allowBComment', true)
-    profileObject.set('showBlog', true)
-    profileObject.set('blogName', '沸点')
+    const profileObject = new AV.Object("CMS_Profile");
+    profileObject.set(
+      "logo",
+      "https://static.ccrgt.com/images/42bc258e-d183-49d5-aea4-6eb5914647e3.png"
+    );
+    profileObject.set("title", "oh my blog");
+    profileObject.set("github", "https://github.com/joe-lz/oh-my-blog");
+    profileObject.set(
+      "copyright",
+      "Copyright © 1998 - 2020 oh my blog. All Rights Reserved"
+    );
+    profileObject.set("allowAComment", true);
+    profileObject.set("allowBComment", true);
+    profileObject.set("showBlog", true);
+    profileObject.set("blogName", "沸点");
     // 将对象保存到云端
     profileObject.save().then(
       (todo) => {
-        resolve(todo)
+        resolve(todo);
       },
       (error) => {
-        reject(err)
+        reject(err);
       }
-    )
-  })
-}
+    );
+  });
+};
 
 export const getProfileList = (params = {}) => {
   return new Promise((resolve, reject) => {
-    const query = new AV.Query('CMS_Profile')
-    query.descending('createdAt')
+    const query = new AV.Query("CMS_Profile");
+    query.descending("createdAt");
     query
       .first()
       .then((res) => {
-        resolve(res)
+        resolve(res);
       })
       .catch((err) => {
-        reject(err)
-      })
-  })
-}
+        reject(err);
+      });
+  });
+};
 
 export const updateProfile = ({ profileItem, params }) => {
   return new Promise((resolve, reject) => {
     Object.keys(params).map((keyname) => {
-      profileItem.set(keyname, params[keyname])
-    })
+      if (keyname === "views" || keyname === "views_uv") {
+        profileItem.increment(keyname, params[keyname]);
+      } else {
+        profileItem.set(keyname, params[keyname]);
+      }
+    });
     profileItem.save().then(
       (res) => {
-        resolve(res)
+        resolve(res);
       },
       (error) => {
         // 异常处理
-        reject(error)
+        reject(error);
       }
-    )
-  })
-}
+    );
+  });
+};
